4c5c252345f2d897a77d9d2f2a2bae1eab87f186
mochenya <mochen20070702@gmail.com>
2024-02-24 17:12:51 +0800

    markw: fixing incorrect characters

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,7 +80,7 @@ TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/soc/7000000.ssusb/7000000.dwc3/
 TW_OVERRIDE_SYSTEM_PROPS := \
     "ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 # Set the default language
-TW_DEFAULT_LANGUAGE := zh-CN
+TW_DEFAULT_LANGUAGE := zh_CN
 
 # TWRP - Crypto
 TW_INCLUDE_CRYPTO := true
